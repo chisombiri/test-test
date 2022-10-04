5b7bfd004be7897f9e23cbb97edb8fd596124fed
chisombiri <chisombiri@gmail.com>
2022-10-04 15:15:13 +0100

    mock testing

--- script2.test.js
+++ script2.test.js
@@ -16,3 +16,24 @@ it("calls swapi to get people with fetch", () => {
     expect(data.results.length).toBeGreaterThan(5);
   });
 });
+
+//mock
+it("getPeople returns count and results", () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5],
+        }),
+    })
+  );
+
+  expect.assertions(3);
+
+  return swapi.getPeopleFetch(mockFetch).then((data) => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
+  });
+});
